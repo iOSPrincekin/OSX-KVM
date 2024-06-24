5f9ad94fb0bc915fa1f1be2369599a0b4afd3a10
LEE <15267030696@163.com>
2024-06-24 15:24:14 +0800

    1.调试 opencore；

--- OpenCore-Boot.sh
+++ OpenCore-Boot.sh
@@ -27,39 +27,39 @@ REPO_PATH="."
 OVMF_DIR="."
 
 # shellcheck disable=SC2054
-args=(
-  -accel hvf -m "$ALLOCATED_RAM" -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,"$MY_OPTIONS"
-  -machine q35
-  -usb -device usb-kbd -device usb-tablet
-  -smp "$CPU_THREADS",cores="$CPU_CORES",sockets="$CPU_SOCKETS"
-  -device usb-ehci,id=ehci
-  # -device usb-kbd,bus=ehci.0
-  # -device usb-mouse,bus=ehci.0
-  -device nec-usb-xhci,id=xhci
-  -global nec-usb-xhci.msi=off
-  # -global ICH9-LPC.acpi-pci-hotplug-with-bridge-support=off
-  # -device usb-host,vendorid=0x8086,productid=0x0808  # 2 USD USB Sound Card
-  # -device usb-host,vendorid=0x1b3f,productid=0x2008  # Another 2 USD USB Sound Card
-  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
-  -drive if=pflash,format=raw,readonly=on,file="$REPO_PATH/$OVMF_DIR/OVMF_CODE.fd"
-  -drive if=pflash,format=raw,file="$REPO_PATH/$OVMF_DIR/OVMF_VARS-1920x1080.fd"
-  -smbios type=2
-  -device ich9-intel-hda -device hda-duplex
-  -device ich9-ahci,id=sata
-  #-drive id=OpenCoreBoot,if=none,snapshot=on,format=qcow2,file="$REPO_PATH/OpenCore/OpenCore.qcow2"
-  -drive file="$REPO_PATH/OpenCore/OpenCore-master.iso",if=none,id=OpenCoreBoot,format=raw,
-  -device ide-hd,bus=sata.2,drive=OpenCoreBoot
-  -device ide-hd,bus=sata.3,drive=InstallMedia
-  -drive id=InstallMedia,if=none,file="$REPO_PATH/BaseSystem.img",format=raw
-  -drive id=MacHDD,if=none,file="$REPO_PATH/mac_hdd_ng.img",format=qcow2
-  -device ide-hd,bus=sata.4,drive=MacHDD
-  # -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
-  -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
-  # -netdev user,id=net0 -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27  # Note: Use this line for High Sierra
-  -serial stdio
-  -device vmware-svga
-  -debugcon file:debug.log -global isa-debugcon.iobase=0x402
-)
+QEMU_ARGS="-s -S -m $ALLOCATED_RAM -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS -machine q35 -usb -device usb-kbd -device usb-tablet -smp $CPU_THREADS,cores=$CPU_CORES,sockets=$CPU_SOCKETS -device usb-ehci,id=ehci -device nec-usb-xhci,id=xhci -global nec-usb-xhci.msi=off -device isa-applesmc,osk='ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc' -drive if=pflash,format=raw,readonly=on,file=$REPO_PATH/$OVMF_DIR/OVMF_CODE.fd -smbios type=2 -device ich9-intel-hda -device hda-duplex -device ich9-ahci,id=sata -drive file=$REPO_PATH/OpenCore/OpenCore-master.iso,if=none,id=OpenCoreBoot,format=raw, -device ide-hd,bus=sata.2,drive=OpenCoreBoot -device ide-hd,bus=sata.3,drive=InstallMedia -drive id=InstallMedia,if=none,file=$REPO_PATH/BaseSystem.img,format=raw -drive id=MacHDD,if=none,file=$REPO_PATH/mac_hdd_ng.img,format=qcow2 -device ide-hd,bus=sata.4,drive=MacHDD -netdev user,id=net0,hostfwd=tcp::2222-:22 -serial stdio -device vmware-svga -debugcon file:debug.log -global isa-debugcon.iobase=0x402"
 
-echo "qemu-system-x86_64 ${args[@]}"
-qemu-system-x86_64 "${args[@]}"
+echo "qemu-system-x86_64 ${QEMU_ARGS}"
+
+# 路径
+BASEDIR=$(dirname "$0")
+cd ${BASEDIR}
+BASEDIR=`pwd`
+
+ROOT_DIR=${BASEDIR}/
+
+KVM_Opencore_Dir=/Users/lee/Desktop/Computer_Systems/UEFI/KVM-Opencore/
+
+KVM_Opencore_EFI_Dir=${KVM_Opencore_Dir}/EFI/
+
+KVM_Opencore_BOOT_Dir=${KVM_Opencore_EFI_Dir}/BOOT/
+
+KVM_Opencore_OC_Dir=${KVM_Opencore_EFI_Dir}/OC/
+
+BOOTx64_efi=${KVM_Opencore_BOOT_Dir}/BOOTx64.efi
+
+OpenCore_efi=${KVM_Opencore_OC_Dir}/OpenCore.efi
+
+Build_dir=/Users/lee/Desktop/Computer_Systems/UEFI/KVM-Opencore/src/OpenCorePkg/UDK/Build/
+
+Bootstrap_dll=${Build_dir}/OpenCorePkg/DEBUG_XCODE5/X64/OpenCorePkg/Application/Bootstrap/Bootstrap/DEBUG/Bootstrap.dll
+
+OpenCore_dll=${Build_dir}/OpenCorePkg/DEBUG_XCODE5/X64/OpenCorePkg/Application/OpenCore/OpenCore/DEBUG/OpenCore.dll
+
+osascript -e "tell application \"Terminal\" to quit"
+osascript -e "tell application \"Terminal\" to do script \"cd ${ROOT_DIR}\\nlldb ${Bootstrap_dll} \\n  file ${Bootstrap_dll} \\n target modules load --file ${Bootstrap_dll} --slide 0 \\n file ${OpenCore_dll} \\n target modules load --file ${OpenCore_dll} --slide 0 \\n gdb-remote localhost:1234 \\n \"" \
+-e "tell application \"Terminal\" to activate" \
+-e "tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down" \
+-e "tell application \"Terminal\" to set background color of window 1 to {0,0,0,1}" \
+-e "tell application \"Terminal\" to do script \"cd ${ROOT_DIR} \\n sleep 0.3\\n qemu-system-x86_64 ${QEMU_ARGS} \\n \\n \" in window 1"
+
